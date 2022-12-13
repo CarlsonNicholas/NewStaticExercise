79a1c3026fd82c3dbbf92733361bd4652eb4836f
Nick Carlson <CarlsonNicholas82@gmail.com>
2022-12-13 15:01:58 -0500

    Completed

---
+++ NewStaticExercise/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+using NewStaticExercise;
+using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine("This program will convert a temperature from Celsius to Fahrenheight or Fahrenheight to Celsius. ");
+Console.WriteLine("");
+Console.WriteLine("To convert from Celsius type 'C' and then the temperature");
+Console.WriteLine("");
+Console.WriteLine("Example 'C' then when prompted '0' - Conversion = 32 Fahrenheight"); 
+Console.WriteLine("");
+Console.WriteLine("To convert from Farhenheight to Celsius type 'F' a space and then the temperature");
+Console.WriteLine("");
+Console.WriteLine("Example 'F' then when prompted '32' - Conversion = 0 Celsius");
+Console.WriteLine("");
+Console.WriteLine("Please type in F or C to start: ");
+Console.WriteLine("");
+
+var userInput = Console.ReadLine();
+
+
+if (userInput.ToUpper() == "F")
+{
+    Console.WriteLine("");
+    Console.WriteLine("You have selected Farhenheight; please type the temperature to convert to Celsius: ");
+    
+    var f_Input = Console.ReadLine();
+    var fc_Input = Double.Parse(f_Input);
+
+    Temp_Converter.FtoC(fc_Input);
+    Console.WriteLine();
+    Console.WriteLine($"Your conversion is {Temp_Converter.FtoC(fc_Input)} degrees celsius");
+
+}
+else if (userInput.ToUpper() == "C")
+{
+    Console.WriteLine("");
+    Console.WriteLine("You have selected Celsius; please type the temperature to convert to Fahrenheight: ");
+    
+    var c_Input = Console.ReadLine();
+    var dc_Input = Double.Parse(c_Input);
+
+    Temp_Converter.CtoF(dc_Input);
+    Console.WriteLine();
+    Console.WriteLine($"Your conversion is {Temp_Converter.CtoF(dc_Input)} degress fahrenheight");
+}
+
+else
+{
+    Console.WriteLine("");
+    Console.WriteLine("That was an invalid response.");
+}
+
---
+++ NewStaticExercise/Temp Converter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewStaticExercise
+{
+    public static class Temp_Converter
+    {
+        public static double FtoC(double F)
+        {
+            double F_conversion = (F - 32) / 1.8;
+
+            return F_conversion;
+        }
+
+        public static double CtoF(double C)
+        {
+            double C_conversion = (C * 9 / 5 + 32);
+           
+            return C_conversion;
+        }
+
+    }
+}
